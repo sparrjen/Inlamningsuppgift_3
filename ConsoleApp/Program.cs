db3a7ddfba3e09719198275a336fb39e1e568669
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-23 21:27:56 +0200

    Inlamningsuppgift_3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,15 +9,18 @@ namespace ConsoleApp
     class Program
     {
 
-        private static readonly string _conn = "";
+        private static readonly string _conn = "HostName=ecwin20-iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=8087yP5qbfLrLRgl7bl7Fn12pLiIGzcO9b8TY62TQCo=";
         
-        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt); 
+        private static readonly DeviceClient deviceClient = 
+            DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt); 
         
         
         static async Task Main(string[] args)
         {
 
-           await DeviceService.SendMessageAsync(deviceClient); 
+           await DeviceService.SendMessageAsync(deviceClient);
+            
+            Console.ReadKey();
          
         }
     }
