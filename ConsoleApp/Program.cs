e11a5c7d8adab0081212157cc2d003af44e73eb3
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-23 14:16:02 +0200

    Inlamningsuppgift_3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static readonly string _conn = "";
+        
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt); 
+        
+        
+        static async Task Main(string[] args)
         {
-            
+
+           await DeviceService.SendMessageAsync(deviceClient); 
+         
         }
     }
 }
---
+++ SharedLibraries/Services/DeviceService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibraries.Models;
+
+namespace SharedLibraries.Services
+{
+    public static class DeviceService
+    {
+        private static readonly Random rnd = new Random();
+
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            
+            while(true)
+            {
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(15, 35),
+                    Humidity = rnd.Next(40, 50)
+                };
+
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent: {json}"); 
+                await Task.Delay(60 * 1000);
+
+            }
+        }
+    }
+}
